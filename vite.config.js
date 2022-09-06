d05dbcaa617432613964c32f1f6b3af5445c5033
Michael Nascimento <michael.nsc@outlook.com>
2022-09-06 10:23:24 -0300

    chore: config path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
@@ -5,4 +6,9 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "./src/components/pages"),
+    },
+  },
 });
