1a212f7cde9c6c949dba1df96048fa87c03e8b5c
Michael Nascimento <michael.nsc@outlook.com>
2022-09-06 11:09:13 -0300

    feat: config initial routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,34 @@
-import { Home } from "@pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
+
+import {
+  DeviceCreate,
+  DeviceDetails,
+  DeviceList,
+  Home,
+  Landing,
+  Login,
+  UserCreate,
+  UserDetails,
+} from "@pages";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<UserCreate />} />
+
+        <Route path="dashboard" element={<PrivateRoute />}>
+          <Route index element={<Home />} />
+          <Route path="profile" element={<UserDetails />} />
+          <Route path="devices">
+            <Route index element={<DeviceList />} />
+            <Route path=":deviceId" element={<DeviceDetails />} />
+            <Route path="new" element={<DeviceCreate />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
