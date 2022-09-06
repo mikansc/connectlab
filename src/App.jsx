b6baf1dc9a81db9805b5ce842538f370d449d693
Michael Nascimento <michael.nsc@outlook.com>
2022-09-06 09:47:29 -0300

    chore: configure globals

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./themes/default";
+import { GlobalStyle } from "./themes/GlobalStyle";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div className="App">Hi</div>
+      <GlobalStyle />
     </ThemeProvider>
   );
 };
---
+++ src/themes/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    font-family: 'Lato', Arial, Helvetica, sans-serif;
+  }
+  
+  body {
+    font-size: 1.6rem;
+  }
+`;
