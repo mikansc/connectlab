5c8ae87a38a417b5fbf828a15d12fa5398be0ef7
Michael Nascimento <michael.nsc@outlook.com>
2022-09-07 08:45:19 -0300

    refactor: change max width rules

--- src/components/organisms/SignupForm/SignupForm.styles.js
+++ src/components/organisms/SignupForm/SignupForm.styles.js
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const StyledContainer = styled.div`
   width: 100%;
-  max-width: 1024px;
-  margin: 0 auto;
 `;
 
 export const StyledFields = styled.div`
--- src/components/pages/Users/Users.styles.js
+++ src/components/pages/Users/Users.styles.js
@@ -2,5 +2,7 @@ import styled from "styled-components";
 
 export const StyledFormWrapper = styled.div`
   width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
   padding: 2em 0;
 `;
