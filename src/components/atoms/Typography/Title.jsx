74cfcfbe20de184f013d69d2488f7b22e2d18425
Michael Nascimento <michael.nsc@outlook.com>
2022-09-07 08:01:24 -0300

    refactor: adjust Title to change alignment

--- src/components/atoms/Typography/Title.jsx
+++ src/components/atoms/Typography/Title.jsx
@@ -2,11 +2,16 @@ import PropTypes from "prop-types";
 
 import { StyledHeading } from "./Typography.styles";
 
-export const Title = ({ children, as }) => {
-  return <StyledHeading as={as}>{children}</StyledHeading>;
+export const Title = ({ children, as, align }) => {
+  return (
+    <StyledHeading as={as} align={align}>
+      {children}
+    </StyledHeading>
+  );
 };
 
 Title.propTypes = {
   as: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  align: PropTypes.oneOf(["center", "left", "right"]),
 };
--- src/components/atoms/Typography/Typography.styles.js
+++ src/components/atoms/Typography/Typography.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledHeading = styled.h1`
-  text-align: center;
+  text-align: ${({ align }) => align ?? "center"};
   font-weight: 700;
   color: ${({ theme }) => theme.colors.main.default};
 `;
