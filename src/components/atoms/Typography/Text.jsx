7978acff61819c3359e3d26c3bc7f2686d4f93ff
Michael Nascimento <michael.nsc@outlook.com>
2022-09-07 09:28:00 -0300

    refactor: add children to text component

--- src/components/atoms/Typography/Text.jsx
+++ src/components/atoms/Typography/Text.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types";
+
 import { StyledText } from "./Typography.styles";
 
-export const Text = () => {
-  return <StyledText>Text</StyledText>;
+export const Text = ({ children }) => {
+  return <StyledText>{children}</StyledText>;
+};
+
+Text.propTypes = {
+  children: PropTypes.node.isRequired,
 };
