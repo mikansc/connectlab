0ebad6df6c128c8c83cfb32b9cfce2271ff81e02
Michael Nascimento <michael.nsc@outlook.com>
2022-09-07 07:45:34 -0300

    feat: create Landing Page

--- src/components/pages/Landing/Landing.jsx
+++ src/components/pages/Landing/Landing.jsx
@@ -1,10 +1,29 @@
-import { Paper } from "@atoms";
-import { StyledContainer } from "./Landing.styles";
+import { Image, Logo } from "@atoms";
+import { LoginForm } from "@organisms";
+import LandingImage from "./assets/img/smart-house.jpg";
+
+import {
+  StyledContainer,
+  StyledFormWrapper,
+  StyledLeftContent,
+  StyledLogoWrapper,
+  StyledRightContent,
+} from "./Landing.styles";
 
 export const Landing = () => {
   return (
     <StyledContainer>
-      <Paper>Landing</Paper>
+      <StyledLeftContent>
+        <Image src={LandingImage} alt="Welcome to the landing page" />
+      </StyledLeftContent>
+      <StyledRightContent>
+        <StyledLogoWrapper>
+          <Logo />
+        </StyledLogoWrapper>
+        <StyledFormWrapper>
+          <LoginForm />
+        </StyledFormWrapper>
+      </StyledRightContent>
     </StyledContainer>
   );
 };
---
+++ src/components/pages/Landing/Landing.styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.background};
+  display: flex;
+`;
+
+export const StyledLeftContent = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.colors.main.default};
+`;
+
+export const StyledRightContent = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const StyledLogoWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledFormWrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 80px);
+  display: grid;
+  place-items: center;
+`;
