a5e5d5c56d1775f8e7847d93cd857e3024e0f0e8
Michael Nascimento <michael.nsc@outlook.com>
2022-09-07 08:28:13 -0300

    feat: create Signup organism

---
+++ src/components/organisms/SignupForm/SignupForm.jsx
@@ -0,0 +1,93 @@
+import { Button, Paper, Title } from "@atoms";
+import { InputField } from "@molecules";
+
+import {
+  StyledFields,
+  StyledContainer,
+  StyledButtons,
+  StyledRow,
+  Separator,
+} from "./SignupForm.styles";
+
+export const SignupForm = () => {
+  return (
+    <StyledContainer>
+      <Paper>
+        <Title as="h2">Cadastrar usuário</Title>
+        <StyledFields>
+          <Title as="h3" align="left">
+            Dados pessoais
+          </Title>
+          <StyledRow>
+            <InputField
+              name="fullName"
+              label="Nome completo"
+              placeholder="digite seu nome completo..."
+            />
+            <InputField name="phone" label="Telefone" placeholder="(XX) XXXXX-XXXX" />
+          </StyledRow>
+
+          <StyledRow>
+            <InputField name="email" label="E-mail" placeholder="usuario@mail.com" />
+
+            <InputField
+              name="photoUrk"
+              label="URL da foto"
+              placeholder="insira a url da foto de perfil..."
+            />
+          </StyledRow>
+
+          <StyledRow>
+            <InputField
+              name="password"
+              type="password"
+              label="Senha"
+              placeholder="digite sua senha..."
+            />
+            <InputField
+              name="passwordConfirm"
+              type="password"
+              label="Confirme a senha"
+              placeholder="digite sua senha novamente..."
+            />
+          </StyledRow>
+
+          <Separator />
+
+          <Title as="h3" align="left">
+            Endereço
+          </Title>
+          <StyledRow columns="3">
+            <InputField name="cep" label="CEP" placeholder="XXXXX-XXX" />
+          </StyledRow>
+          <StyledRow>
+            <InputField
+              name="address1"
+              label="Logradouro / Endereço"
+              placeholder="digite o endereço com número..."
+            />
+            <InputField name="address2" label="Complemento" placeholder="digite o complemento..." />
+          </StyledRow>
+          <StyledRow>
+            <InputField name="city" label="Cidade" placeholder="digite a cidade..." />
+            <InputField name="state" label="Estado" placeholder="digite o estado..." />
+          </StyledRow>
+
+          <Separator />
+
+          <Title as="h3" align="left">
+            Identificação do local
+          </Title>
+          <StyledRow>
+            <InputField name="local.type" label="Tipo de local" placeholder="escolha..." />
+            <InputField name="local.rooms" label="Tipo de local" placeholder="escolha..." />
+          </StyledRow>
+        </StyledFields>
+        <StyledButtons>
+          <Button onClick={() => {}}>Cadastrar</Button>
+          <Button onClick={() => {}}>Cancelar</Button>
+        </StyledButtons>
+      </Paper>
+    </StyledContainer>
+  );
+};
---
+++ src/components/organisms/SignupForm/SignupForm.styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  margin: 0 auto;
+`;
+
+export const StyledFields = styled.div`
+  width: 100%;
+  margin: 1em 0;
+`;
+
+export const StyledRow = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(${({ columns }) => columns || 2}, 1fr);
+  column-gap: 1em;
+`;
+
+export const Separator = styled.hr`
+  width: 100%;
+  border: 1px solid ${({ theme }) => theme.colors.main.black};
+  margin: 1em 0;
+  opacity: 0.1;
+`;
+
+export const StyledButtons = styled.div`
+  width: 100%;
+  margin-top: 1.5em;
+  display: flex;
+  justify-content: space-evenly;
+`;
