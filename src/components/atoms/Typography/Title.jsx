f0d3d55555dc17f3c2fd220d1b585c8e59154bd2
Michael Nascimento <michael.nsc@outlook.com>
2022-09-08 06:25:11 -0300

    refactor: add variants to Title component

--- src/components/atoms/Typography/Title.jsx
+++ src/components/atoms/Typography/Title.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 import { StyledHeading } from "./Typography.styles";
 
-export const Title = ({ children, as, align }) => {
+export const Title = ({ children, as, align, variant = "main" }) => {
   return (
-    <StyledHeading as={as} align={align}>
+    <StyledHeading as={as} align={align} variant={variant}>
       {children}
     </StyledHeading>
   );
@@ -12,6 +12,7 @@ export const Title = ({ children, as, align }) => {
 
 Title.propTypes = {
   as: PropTypes.string.isRequired,
+  variant: PropTypes.oneOf(["main", "secondary"]),
   children: PropTypes.node.isRequired,
   align: PropTypes.oneOf(["center", "left", "right"]),
 };
--- src/components/atoms/Typography/Typography.styles.js
+++ src/components/atoms/Typography/Typography.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledHeading = styled.h1`
   text-align: ${({ align }) => align ?? "center"};
   font-weight: 700;
-  color: ${({ theme }) => theme.colors.main.default};
+  color: ${({ theme, variant }) => theme.colors[variant].default};
 `;
 
 export const StyledText = styled.span`
