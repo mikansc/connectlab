fdb49648fc75c9452faccc89e8eda068d3b88b50
Michael Nascimento <michael.nsc@outlook.com>
2022-09-08 06:26:15 -0300

    feat: add DeviceList to Home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import { WeatherHero } from "@organisms";
+import { DeviceList, WeatherHero } from "@organisms";
 import { DefaultPage } from "@templates";
 
 export const Home = () => {
   return (
     <DefaultPage>
       <WeatherHero />
+      <DeviceList />
     </DefaultPage>
   );
 };
