1682b3d59c2f028b9559759598074d7080f48b47
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 04:26:45 -0300

    feat: create authContext base

---
+++ src/contexts/auth-context.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { useAuthentication } from "@hooks";
+
+const context = React.createContext(null);
+const ContextProvider = context.Provider;
+
+export const AuthProvider = ({ children }) => {
+  const { isLoggedIn, signIn, signOut } = useAuthentication();
+
+  return <ContextProvider value={{ isLoggedIn, signIn, signOut }}>{children}</ContextProvider>;
+};
+
+export const useAuthContext = () => {
+  const ctx = React.useContext(context);
+  if (!ctx) throw new Error("useAuthContext can only be used inside AuthProvider");
+  return ctx;
+};
