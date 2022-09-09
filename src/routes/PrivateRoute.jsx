d34ad15d8fef5543e0a99ec7dbb7ea9ec8b6cf02
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 04:27:00 -0300

    feat: add logged in verification at Private Routes

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,9 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useAuthContext } from "@contexts";
 
 export const PrivateRoute = () => {
   // TODO - implement authentication logic
-  return <Outlet />;
+  const { isLoggedIn } = useAuthContext();
+
+  return isLoggedIn ? <Outlet /> : <Navigate to="/" />;
 };
