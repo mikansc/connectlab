511371c67a14773559b135d4c3941a4d8768625d
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 04:34:25 -0300

    feat: add form and signin to LoginForm

--- src/components/organisms/LoginForm/LoginForm.jsx
+++ src/components/organisms/LoginForm/LoginForm.jsx
@@ -3,23 +3,32 @@ import { Link } from "react-router-dom";
 import { Button, Paper, Title } from "@atoms";
 import { ButtonGroup, InputField } from "@molecules";
 import { StyledLoginFields, StyledContainer } from "./LoginForm.styles";
+import { useAuthContext } from "@contexts";
+import { useForm } from "react-hook-form";
 
 export const LoginForm = () => {
+  const { signIn } = useAuthContext();
+  const { register, handleSubmit } = useForm();
+
+  const handleSignIn = (data) => {
+    signIn(data);
+  };
+
   return (
     <StyledContainer>
       <Paper>
         <Title as="h2">Acessar</Title>
         <StyledLoginFields>
-          <InputField name="username" label="Usuário" placeholder="usuario@mail.com" />
+          <InputField {...register("username")} label="Usuário" placeholder="usuario@mail.com" />
           <InputField
-            name="password"
+            {...register("password")}
             type="password"
             label="Senha"
             placeholder="digite sua senha..."
           />
         </StyledLoginFields>
         <ButtonGroup>
-          <Button onClick={() => {}}>Entrar</Button>
+          <Button onClick={handleSubmit(handleSignIn)}>Entrar</Button>
           <Button as={Link} to="/signup">
             Cadastrar
           </Button>
