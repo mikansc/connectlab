b98282475389a29b6f7304c3ba9300746521e921
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 08:59:51 -0300

    fix: remove comment

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,7 +2,6 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useAuthContext } from "@contexts";
 
 export const PrivateRoute = () => {
-  // TODO - implement authentication logic
   const { isLoggedIn } = useAuthContext();
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/" />;
