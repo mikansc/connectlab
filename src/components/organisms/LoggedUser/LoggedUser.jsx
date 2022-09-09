c06e435f3f16f164879e848307864992068e7ded
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 09:46:21 -0300

    refactor: add hover fx on the LoggedUser component

--- src/components/organisms/LoggedUser/LoggedUser.jsx
+++ src/components/organisms/LoggedUser/LoggedUser.jsx
@@ -1,17 +1,30 @@
 import { Avatar } from "@molecules";
-import { StyledClock, StyledUserData, StyledUserName, StyledWrapper } from "./LoggedUser.styles";
+import { useNavigate } from "react-router-dom";
+import {
+  StyledClock,
+  StyledContainer,
+  StyledUserData,
+  StyledUserName,
+  StyledWrapper,
+} from "./LoggedUser.styles";
 const formatDate = (date) => {
   return Intl.DateTimeFormat("pt-BR", { dateStyle: "full" }).format(date);
 };
 
 export const LoggedUser = () => {
+  const navigate = useNavigate();
+  const goToUserProfile = () => {
+    navigate("/dashboard/profile");
+  };
   return (
-    <StyledWrapper>
-      <Avatar imageUrl="https://github.com/mikansc.png" size="small" />
-      <StyledUserData>
-        <StyledUserName>Olá, Michael Nascimento</StyledUserName>
-        <StyledClock>{formatDate(Date.now())}</StyledClock>
-      </StyledUserData>
+    <StyledWrapper onClick={goToUserProfile}>
+      <StyledContainer>
+        <Avatar imageUrl="https://github.com/mikansc.png" size="small" />
+        <StyledUserData>
+          <StyledUserName>Olá, Michael Nascimento</StyledUserName>
+          <StyledClock>{formatDate(Date.now())}</StyledClock>
+        </StyledUserData>
+      </StyledContainer>
     </StyledWrapper>
   );
 };
--- src/components/organisms/LoggedUser/LoggedUser.styles.js
+++ src/components/organisms/LoggedUser/LoggedUser.styles.js
@@ -2,14 +2,29 @@ import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: flex-end;
+`;
+
+export const StyledContainer = styled.div`
+  height: 100%;
+  display: flex;
   align-items: center;
   padding: 0 1em;
-  margin-right: 3em;
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.main.text};
+    background-color: ${({ theme }) => theme.colors.main.light};
+  }
 `;
 
 export const StyledUserData = styled.div`
+  display: flex;
+  justify-content: center;
+
+  height: 100%;
   margin: 0 0.5em;
   display: flex;
   flex-direction: column;
