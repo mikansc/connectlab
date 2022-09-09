b322f7afa8800f811e68c4def4f691320c2ab549
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 04:27:24 -0300

    feat: add redirection at Landing page if logged in

--- src/components/pages/Landing/Landing.jsx
+++ src/components/pages/Landing/Landing.jsx
@@ -1,5 +1,8 @@
 import { Image, Logo } from "@atoms";
+import { useAuthContext } from "@contexts";
 import { LoginForm } from "@organisms";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import LandingImage from "./assets/img/smart-house.jpg";
 
 import {
@@ -11,10 +14,17 @@ import {
 } from "./Landing.styles";
 
 export const Landing = () => {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuthContext();
+
+  useEffect(() => {
+    if (isLoggedIn) navigate("/dashboard");
+  }, [isLoggedIn, navigate]);
+
   return (
     <StyledContainer>
       <StyledLeftContent>
-        <Image src={LandingImage} alt="Welcome to the landing page" />
+        <Image src={LandingImage} alt="ConnectLab Login page" />
       </StyledLeftContent>
       <StyledRightContent>
         <StyledLogoWrapper>
