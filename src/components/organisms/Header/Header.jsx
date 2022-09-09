87ae76a760117a3a1bebbcb60f4ae9b9ac32d22e
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 09:22:01 -0300

    refactor: change Header to use Navigation molecule

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { Logo } from "@atoms";
+import { Navigation } from "@molecules";
 import { StyledContainer } from "./Header.styles";
 
 export const Header = () => {
   return (
     <StyledContainer>
       <Logo />
+      <Navigation />
     </StyledContainer>
   );
 };
--- src/components/organisms/Header/Header.styles.js
+++ src/components/organisms/Header/Header.styles.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  width: 100%;
   height: 60px;
+  padding: 0 2em;
   background-color: ${({ theme }) => theme.colors.main.default};
   box-shadow: ${({ theme }) => theme.shadow.big};
   border-bottom: 1px solid ${({ theme }) => theme.colors.main.dark};
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
