57dcf3944a99469c62b95d23cf3bb7e95c6fec86
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 09:46:33 -0300

    feat: add LoggedUser component into Header

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { Logo } from "@atoms";
 import { Navigation } from "@molecules";
+import { LoggedUser } from "../LoggedUser";
 import { StyledContainer } from "./Header.styles";
 
 export const Header = () => {
   return (
     <StyledContainer>
       <Logo />
+      <LoggedUser />
       <Navigation />
     </StyledContainer>
   );
