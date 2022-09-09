d2ea79deec6e595b86ba0a4e7dc04d351e1efe80
Michael Nascimento <michael.nsc@outlook.com>
2022-09-09 03:48:37 -0300

    feat: add devices rendering to DeviceLIst

--- src/components/pages/Devices/DeviceList.jsx
+++ src/components/pages/Devices/DeviceList.jsx
@@ -1,3 +1,22 @@
+import { Separator, Title } from "@atoms";
+import { Device, Filter } from "@molecules";
+import { DeviceGrid } from "@organisms";
+import { DefaultPage } from "@templates";
+import { devices } from "../../../mock/devices";
+
 export const DeviceList = () => {
-  return <div>DeviceList</div>;
+  return (
+    <DefaultPage>
+      <Title as="h2" align="left">
+        Adicionar dispositivo
+      </Title>
+      <Separator />
+      <Filter />
+      <DeviceGrid title="Todos os dispositivos">
+        {devices.map((device) => (
+          <Device key={device.id} device={device} />
+        ))}
+      </DeviceGrid>
+    </DefaultPage>
+  );
 };
