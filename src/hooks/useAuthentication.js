895c8e8ba1dd6234a13cdeae40da7a4d8d503c2c
Michael Nascimento <michael.nsc@outlook.com>
2022-09-10 10:14:56 -0300

    feat: save and retrieve user from session

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,18 +1,29 @@
-import { useState } from "react";
-import { login } from "@services";
+import { useEffect, useState } from "react";
+import { login, storageService } from "@services";
 import { useAppContext } from "@contexts";
 import { StatusTypes } from "@utils";
+
+const query = "usr";
 
 export const useAuthentication = () => {
   const [user, setUser] = useState(null);
   const { setStatus } = useAppContext();
+
+  useEffect(() => {
+    setStatus(StatusTypes.loading);
+    const user = storageService.get(query);
+    if (user) setUser(user);
+    setStatus(StatusTypes.success);
+  }, [setStatus]);
 
   const signIn = ({ username, password }) => {
     setStatus(StatusTypes.loading);
     login({ username, password })
       .then(({ data }) => {
         const { accessToken, user } = data;
-        setUser({ accessToken, ...user });
+        const preparedData = { accessToken, ...user };
+        storageService.save(query, preparedData);
+        setUser(preparedData);
         setStatus(StatusTypes.success);
       })
       .catch((err) => {
@@ -22,6 +33,7 @@ export const useAuthentication = () => {
   };
 
   const signOut = () => {
+    storageService.clear(query);
     setUser(null);
   };
 
--- src/services/local/storageService.js
+++ src/services/local/storageService.js
@@ -1,14 +1,21 @@
-const key = "connlab";
+const key = "connlab_";
 
-const save = (data) => {
-  localStorage.setItem(key, JSON.stringify(data));
+const save = (resource, data) => {
+  sessionStorage.setItem(key.concat(resource), JSON.stringify(data));
 };
 
-const get = () => {
-  localStorage.getItem(key);
+const get = (resource) => {
+  const data = sessionStorage.getItem(key.concat(resource));
+  if (data) return JSON.parse(data);
+  return null;
+};
+
+const clear = (resource) => {
+  sessionStorage.removeItem(key.concat(resource));
 };
 
 export const storageService = {
   save,
   get,
+  clear,
 };
