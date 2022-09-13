a2ff78fc20568a3e1f797a5d8a15a52b10895429
Michael Nascimento <michael.nsc@outlook.com>
2022-09-13 15:43:38 -0300

    refactor: add styles to button container at userForm

--- src/components/organisms/UserForm/UserForm.styles.js
+++ src/components/organisms/UserForm/UserForm.styles.js
@@ -4,9 +4,14 @@ export const StyledContainer = styled.div`
   width: 100%;
 `;
 
-export const StyledFields = styled.div`
+export const StyledFields = styled.form`
   width: 100%;
   margin: 1em 0;
+`;
+
+export const StyledButtonContainer = styled.div`
+  margin: 1em 0;
+  align-self: flex-end;
 `;
 
 export const StyledRow = styled.div`
