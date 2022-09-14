852e5277c5377838214bd230e6418fec1f74d89f
Michael Nascimento <michael.nsc@outlook.com>
2022-09-14 15:08:18 -0300

    refactor: load user data from context to UserDetails

--- src/components/organisms/UserDetails/UserDetails.jsx
+++ src/components/organisms/UserDetails/UserDetails.jsx
@@ -1,41 +1,43 @@
 import { Link } from "react-router-dom";
 
-import { Button, Paper, Pill, Separator, Text, Title } from "@atoms";
+import { Button, Paper, Separator, Text, Title } from "@atoms";
 import { Avatar, ButtonGroup, Property } from "@molecules";
 
 import { StyledContainer, StyledUserDataContainer } from "./UserDetails.styles";
+import { useAuthContext } from "@contexts";
 
 export const UserDetails = () => {
+  const { user } = useAuthContext();
+
+  const { fullName, email, photoUrl, userAddress, phone } = user;
+  const { street, number, city, neighborhood, state, zipCode } = userAddress;
+
   return (
     <Paper>
       <Title as="h2">Perfil do usuário</Title>
       <StyledContainer>
-        <Avatar size="large" imageUrl="https://github.com/mikansc.png" name="Michael" />
+        <Avatar size="large" imageUrl={photoUrl} name={fullName} />
         <StyledUserDataContainer>
           <Title as="h4" align="left">
-            Michael Nascimento
+            {fullName}
           </Title>
-          <Text>michael@email.com - (99) 9999-9999</Text>
+          <Text>
+            {email} - {phone}
+          </Text>
         </StyledUserDataContainer>
       </StyledContainer>
 
       <Separator />
-      <Title as="h3" align="left">
-        Endereço
-      </Title>
-      <Property title="Endereço" value="Rua 123" />
-      <Property title="Complemento" value="Casa" />
-      <Property title="Bairro" value="Jardim alguma coisa" />
-      <Property title="Cidade" value="Joinville" />
-      <Property title="UF" value="Santa Catarina" />
+      <Title as="h3">Endereço</Title>
+      <Property title="Endereço" value={street} />
+      <Property title="Número" value={number} />
+      <Property title="Bairro" value={neighborhood} />
+      <Property title="Cidade" value={city} />
+      <Property title="Estado" value={state} />
+      <Property title="CEP" value={zipCode} />
 
       <Separator />
-      <Title as="h3" align="left">
-        Ambientes
-      </Title>
-      <Property title="Casa">
-        <Pill>Quarto</Pill>
-      </Property>
+
       <ButtonGroup>
         <Button as={Link} to="/dashboard">
           Voltar
