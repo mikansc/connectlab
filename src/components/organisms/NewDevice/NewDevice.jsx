bd40e40c48351d83e5975ddb241242cd5bb7b5b7
Michael Nascimento <michael.nsc@outlook.com>
2022-09-18 20:33:20 -0300

    refactor: add save device to user at NewDevice

--- src/components/organisms/NewDevice/NewDevice.jsx
+++ src/components/organisms/NewDevice/NewDevice.jsx
@@ -1,37 +1,52 @@
 import { useFetchLocals } from "@hooks";
-import { useAppContext } from "@contexts";
+import { useAppContext, useDevicesContext } from "@contexts";
 import { Button, Paper, Separator, Title } from "@atoms";
-import { Avatar, ButtonGroup, InputField, SelectField } from "@molecules";
+import { Avatar, ButtonGroup, InputField, SelectField, ToggleField } from "@molecules";
 
 import { StyledCenteredAvatar, StyledContainer } from "./NewDevice.styles";
+import { useForm } from "react-hook-form";
 
 export const NewDevice = () => {
   const { modal, status } = useAppContext();
   const { locals } = useFetchLocals();
+  const { addDeviceToUser } = useDevicesContext();
+  const { register, handleSubmit } = useForm({
+    defaultValues: { is_on: true },
+  });
 
-  const { name, photoUrl } = modal.data;
-
+  const { name, photoUrl, _id } = modal.data;
+  console.log(modal.data._id);
   return (
     <StyledContainer>
       <Paper>
-        <StyledCenteredAvatar>
-          <Avatar imageUrl={photoUrl} name={name} size="large" />
-        </StyledCenteredAvatar>
-        <Title as="h3">{name}</Title>
-        <Separator />
-        <SelectField
-          label="Local"
-          name="name"
-          placeholder="Selecione o local..."
-          disabled={status.isLoading}
-          options={status.isLoading ? ["carregando"] : locals}
-        />
-        <InputField name="comodo" label="Cômodo" placeholder="Quarto, sala, cozinha, etc..." />
-        <Separator />
-        <ButtonGroup>
-          <Button>Salvar</Button>
-          <Button onClick={() => modal.close()}>Cancelar</Button>
-        </ButtonGroup>
+        <form onSubmit={handleSubmit(addDeviceToUser)}>
+          <StyledCenteredAvatar>
+            <Avatar imageUrl={photoUrl} name={name} size="large" />
+          </StyledCenteredAvatar>
+          <Title as="h3">{name}</Title>
+          <Separator />
+          <input type="hidden" {...register("device", { value: _id })} />
+          <SelectField
+            label="Local"
+            {...register("local")}
+            placeholder="Selecione o local..."
+            disabled={status.isLoading}
+            options={status.isLoading ? ["carregando"] : locals}
+          />
+          <InputField
+            {...register("room")}
+            label="Cômodo"
+            placeholder="Quarto, sala, cozinha, etc..."
+          />
+          <ToggleField {...register("is_on")} label="Ligado?" />
+          <Separator />
+          <ButtonGroup>
+            <Button disabled={status.isLoading} type="submit">
+              Salvar
+            </Button>
+            <Button onClick={() => modal.close()}>Cancelar</Button>
+          </ButtonGroup>
+        </form>
       </Paper>
     </StyledContainer>
   );
