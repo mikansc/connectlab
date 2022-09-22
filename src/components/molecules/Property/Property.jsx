163b423fd2a9d71e291e9b325f5439e09cd1a90c
Michael Nascimento <michael.nsc@outlook.com>
2022-09-22 09:41:49 -0300

    refactor: change styled components names

--- src/components/molecules/Property/Property.jsx
+++ src/components/molecules/Property/Property.jsx
@@ -1,13 +1,17 @@
 import PropTypes from "prop-types";
 
-import { StyledContainer, StyledTitle, StyledValue } from "./Property.styles";
+import {
+  StyledPropertyContainer,
+  StyledPropertyTitle,
+  StyledPropertyValue,
+} from "./Property.styles";
 
 export const Property = ({ title, value, children }) => {
   return (
-    <StyledContainer>
-      <StyledTitle>{title}: </StyledTitle>
-      <StyledValue>{children || value}</StyledValue>
-    </StyledContainer>
+    <StyledPropertyContainer>
+      <StyledPropertyTitle>{title}: </StyledPropertyTitle>
+      <StyledPropertyValue>{children || value}</StyledPropertyValue>
+    </StyledPropertyContainer>
   );
 };
 
--- src/components/molecules/Property/Property.styles.js
+++ src/components/molecules/Property/Property.styles.js
@@ -1,15 +1,14 @@
 import styled from "styled-components";
 
-export const StyledContainer = styled.div`
+export const StyledPropertyContainer = styled.div`
+  font-size: 1.5rem;
   line-height: 1.65;
   margin: 0 0.5em;
 `;
 
-export const StyledTitle = styled.strong`
-  font-size: 1.5rem;
+export const StyledPropertyTitle = styled.strong`
   font-weight: 400;
 `;
-export const StyledValue = styled.span`
-  font-size: 1.5rem;
+export const StyledPropertyValue = styled.span`
   font-weight: 300;
 `;
