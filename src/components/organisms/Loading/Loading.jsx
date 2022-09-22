78da2613bde9fda5946eef2792467eeaf89cfdf2
Michael Nascimento <michael.nsc@outlook.com>
2022-09-22 20:25:34 -0300

    fix: adjust loading message

--- src/components/organisms/Loading/Loading.jsx
+++ src/components/organisms/Loading/Loading.jsx
@@ -1,6 +1,7 @@
 import { useAppContext } from "@contexts";
 import { Spinner, Text } from "@atoms";
 import { Modal } from "@organisms";
+import styled from "styled-components";
 
 export const Loading = () => {
   const { status } = useAppContext();
@@ -8,7 +9,14 @@ export const Loading = () => {
   return (
     <Modal open={status.isLoading} zIndex="1100">
       <Spinner />
-      <Text>Aguarde</Text>
+      <TextContainer>
+        <Text>Aguarde</Text>
+      </TextContainer>
     </Modal>
   );
 };
+
+const TextContainer = styled.div`
+  width: 100%;
+  text-align: center;
+`;
