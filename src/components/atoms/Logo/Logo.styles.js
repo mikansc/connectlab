da03d67608567fbd4f3d9ebd1866526341e83f99
Michael Nascimento <michael.nsc@outlook.com>
2022-09-22 13:57:02 -0300

    styles: adjust logo styles

--- src/components/atoms/Logo/Logo.styles.js
+++ src/components/atoms/Logo/Logo.styles.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  width: 140px;
-  height: 60px;
+  width: 120px;
   display: flex;
   align-items: center;
-  padding: 0 2em;
 
   & img {
     display: block;
     width: 100%;
+  }
+
+  @media screen and (${({ theme }) => theme.breakpoints.medium}) {
+    width: 140px;
+    padding: 0 2em;
   }
 `;
