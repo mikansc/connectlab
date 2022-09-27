837ea86e12318e80291d39ef9f205cd1296aa242
Michael Nascimento <michael.nsc@outlook.com>
2022-09-27 16:48:15 -0300

    test: add more Button test

--- src/components/atoms/Button/Button.test.jsx
+++ src/components/atoms/Button/Button.test.jsx
@@ -9,7 +9,20 @@ describe("@atom/Button", () => {
   });
 
   it("should be disabled when disable=true", () => {
-    render(<Button>text</Button>);
-    expect(screen.getByText(/text/)).toBeInTheDocument();
+    render(<Button disabled>text</Button>);
+    const button = screen.getByText(/text/);
+    expect(button).toHaveAttribute("disabled");
+  });
+
+  it("should be full width when fullWidth=true", () => {
+    render(<Button fullWidth>text</Button>);
+    const button = screen.getByText(/text/);
+    expect(button).toHaveStyle("width: 100%;");
+  });
+
+  it("should be auto width when fullWidth=false", () => {
+    render(<Button fullWidth={false}>text</Button>);
+    const button = screen.getByText(/text/);
+    expect(button).toHaveStyle("width: auto;");
   });
 });
