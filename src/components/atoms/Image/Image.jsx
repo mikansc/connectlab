bc3679c7f4d1223cbb3db48cd33eebdd820d4f03
Michael Nascimento <michael.nsc@outlook.com>
2022-09-27 17:15:14 -0300

    refactor: force Image to receive src and alt props

--- src/components/atoms/Image/Image.jsx
+++ src/components/atoms/Image/Image.jsx
@@ -1,3 +1,11 @@
+import PropTypes from "prop-types";
 import { StyledImage } from "./Image.styles";
 
-export const Image = (props) => <StyledImage {...props} data-testId="app-image" />;
+export const Image = ({ src, alt, ...props }) => (
+  <StyledImage src={src} alt={alt} {...props} data-testId="app-image" />
+);
+
+Image.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
