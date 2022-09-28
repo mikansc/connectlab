59248a6c2249c8938140bf948d4dd0c31d2f64c9
Michael Nascimento <michael.nsc@outlook.com>
2022-09-27 23:32:05 -0300

    test: add ButtonGroup test

---
+++ src/components/atoms/Button/ButtonGroup.test.jsx
@@ -0,0 +1,21 @@
+import { screen } from "@testing-library/react";
+import { render } from "@utils";
+import { ButtonGroup } from "./ButtonGroup";
+
+describe("@atom/ButtonGroup", () => {
+  it("should render with children", () => {
+    render(<ButtonGroup>text</ButtonGroup>);
+    expect(screen.getByText(/text/)).toBeInTheDocument();
+  });
+
+  it("should match snapshot", () => {
+    render(<ButtonGroup>Button Group</ButtonGroup>);
+    expect(screen.getByText(/Button Group/)).toMatchInlineSnapshot(`
+      <div
+        class="sc-bczRLJ jCYKrb"
+      >
+        Button Group
+      </div>
+    `);
+  });
+});
