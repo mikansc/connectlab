6c7647bd3d7dd8606e9de75fbcf5add42bce5b10
Michael Nascimento <michael.nsc@outlook.com>
2023-01-05 21:01:38 -0300

    feat: initialize react i18n

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./App";
 import "./assets/css/reset.css";
 import "react-toastify/dist/ReactToastify.min.css";
 
+import "./configs/i18n";
+
 const app = ReactDOM.createRoot(document.getElementById("root"));
 
 app.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="loading...">
+      <App />
+    </Suspense>
   </React.StrictMode>,
 );
