8d45021beb4c0be03c9b241e0d405d4ca6762ab8
Michael Nascimento <michael.nsc@outlook.com>
2023-01-12 04:57:39 -0300

    fix: fix formatter tests

--- src/i18n/formatters/__tests__/full_date.test.js
+++ src/i18n/formatters/__tests__/full_date.test.js
@@ -1,21 +1,23 @@
 import { formatDate } from "../full_date";
 
+const DATE_DEFAULT = "2023-01-01T12:00:00Z"; // 12:00 -> workaround de timezone
+
 describe("formatDate", () => {
   it("should format date in pt-BR", () => {
-    const date = new Date("2023-01-02T00:00:00.000Z");
+    const date = new Date(DATE_DEFAULT);
     const result = formatDate(date, "pt-BR");
-    expect(result).toEqual("domingo, 1 de janeiro de 2023");
+    expect(result).toBe("domingo, 1 de janeiro de 2023");
   });
 
   it("should format date in en-US", () => {
-    const date = new Date("2023-01-02T00:00:00.000Z");
+    const date = new Date(DATE_DEFAULT);
     const result = formatDate(date, "en-US");
-    expect(result).toEqual("Sunday, January 1, 2023");
+    expect(result).toBe("Sunday, January 1, 2023");
   });
 
   it("should format date in es-ES", () => {
-    const date = new Date("2023-01-02T00:00:00.000Z");
+    const date = new Date(DATE_DEFAULT);
     const result = formatDate(date, "es-ES");
-    expect(result).toEqual("domingo, 1 de enero de 2023");
+    expect(result).toBe("domingo, 1 de enero de 2023");
   });
 });
